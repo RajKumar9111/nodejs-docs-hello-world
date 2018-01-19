89c1e0274d214cab03b9fe301bd6bda9fe82ed57
murughan palaniachari <murughan1985@gmail.com>
2018-01-19 20:00:38 +0530

    changed welcome message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Sample App through Azure DevOps Project");
 
 });
 
